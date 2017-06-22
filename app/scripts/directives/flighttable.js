8bdc5d78f808425ec423dd2bdbbe1027dd483856
aman395jain <er.amanjain395@gmail.com>
2017-06-22 22:37:29 +0530

    committed by 22 June

---
+++ app/scripts/directives/flighttable.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name myAssignmentTaskApp.directive:flightTable
+ * @description
+ * # flightTable
+ */
+angular.module('myAssignmentTaskApp')
+  .directive('flightTable', function (flightSearchService) {
+    return {
+      templateUrl:'../views/flighttable.html',
+      restrict: 'E',
+      scope:{
+        airportNameDir:'&'
+
+      },
+      link: function postLink(scope, element, attrs) {
+        flightSearchService.then(function (response) {
+          scope.airportNameDir = response.flightAirportName;
+        })
+      }
+    };
+  });
