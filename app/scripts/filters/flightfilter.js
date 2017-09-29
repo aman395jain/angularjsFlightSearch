431ae29678c1248c183aec22d372ed3b1a17bf4d
aman395jain <er.amanjain395@gmail.com>
2017-09-29 20:38:25 +0530

    committed by 29/09

--- app/scripts/filters/flightfilter.js
+++ app/scripts/filters/flightfilter.js
@@ -10,7 +10,23 @@
  */
 angular.module('myAssignmentTaskApp')
   .filter('flightFilter', function () {
-    return function (input) {
-      return 'flightFilter filter: ' + input;
+    return function (input,airportNames) {
+        var output=[];
+
+        if(angular.isUndefined(airportNames)) {
+          output = input;
+        } else {
+          angular.forEach(input, function (item) {
+            if(item.match(airportNames.ignoreCase)){
+              if (input.includes(item)){
+                output.push(input);
+              }
+            } if(angular.isUndefined(airportNames)) {
+              console.log("the value of item:");
+              output = input;
+            }
+          });
+        }
+        return output;
     };
   });
